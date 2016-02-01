258549c256ae4f2de73b4935882831fb63c8b2fc
jirzin <jirkarous@gmail.com>
2016-02-01 22:04:00 +0000

    initial commit

---
+++ test/galery_dungeon/core_test.clj
@@ -0,0 +1,7 @@
+(ns galery-dungeon.core-test
+  (:require [clojure.test :refer :all]
+            [galery-dungeon.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
